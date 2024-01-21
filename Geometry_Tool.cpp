4b4cc4564a54efb994fe1420982c3879b2127ba4
Marina-D-G <147334873+Marina-D-G@users.noreply.github.com>
2024-01-21 10:28:44 +0200

    Update Geometry_Tool.cpp
    Add functions for special lines in a triangle

--- Geometry_Tool.cpp
+++ Geometry_Tool.cpp
@@ -79,6 +79,63 @@ bool isLineExist(Line* lineRecord, Line& line_temp, int& filledLines)
 	return false;
 }
 
+bool isTriangleExist(Point point1, Point point2, Point point3)
+//we can find out if triangle exists by calculating its area, if it is zero-it doesn't exist
+{
+	double area = 0.5*(point1.x*(point2.y-point3.y) + point2.x * (point3.y - point1.y) + point3.x * (point1.y - point2.y));
+	return area;
+}
+
+Line lineThroughTwoPoints(Point point1, Point point2)
+{
+	Line line_temp{};
+	line_temp.m = (point2.y - point1.y) / (point2.x - point1.x);
+	line_temp.a = point1.y - point2.y;
+	line_temp.b = point2.x - point1.x;
+	line_temp.c = point1.x * point2.y - point2.x * point1.y;
+	return line_temp;
+}
+
+Line altitude(Point p1, Point p2, Point h)
+//the altitude is from h to the line that goes through p1 and p2
+{
+	Line line_temp = lineThroughTwoPoints(p1, p2); 
+	Line altitude {};
+	altitude.m = -1.0 / line_temp.m;
+	altitude.a = altitude.m;
+	altitude.b = 1.0;
+	altitude.c = h.y - altitude.m * h.x;
+	return altitude;
+}
+
+Line median(Point p1, Point p2, Point m)
+//the median is from m to the midpoint between p1 and p2
+{
+	Point midpoint{};
+	midpoint.x = (p1.x + p2.x) / 2.0;
+	midpoint.y = (p1.y + p2.y) / 2.0;
+	Line median = lineThroughTwoPoints(midpoint, m);
+	return median;
+}
+
+Line perpBisector(Point p1, Point p2)
+//the perpendicular bisector is the line that is perpendicular to the line through p1 and p2 and goes through its midpoint
+{
+	Line line_temp = lineThroughTwoPoints(p1, p2);
+	Point midpoint{};
+	midpoint.x = (p1.x + p2.x) / 2.0;
+	midpoint.y = (p1.y + p2.y) / 2.0;
+
+	Line perpBisector{};
+	perpBisector.m = -1.0 / line_temp.m;
+	perpBisector.k = midpoint.y - perpBisector.m * midpoint.x;
+	perpBisector.a = -perpBisector.m;
+	perpBisector.b = 1.0;
+	perpBisector.c = -perpBisector.k;
+
+	return perpBisector;
+}
+
 void inputPoint(Point* pointRecord, int& filledPoints) //define a geometric point with its coordinates
 {
 	if (!pointRecord) //check for nullptr
@@ -151,6 +208,28 @@ Point choosePoint(Point* pointRecord, int& filledPoints)
 	return point_temp;
 }
 
+void chooseThreeValidPoints(Point* pointRecord, int& filledPoints, Point& point1, Point& point2, Point& point3)
+{
+	point1 = choosePoint(pointRecord, filledPoints);
+	switch (filledPoints) //to choose 3 valid points
+	{
+	case 1:
+		inputPoint(pointRecord, filledPoints);
+		point2 = pointRecord[1];
+		inputPoint(pointRecord, filledPoints);
+		point3 = pointRecord[2];
+		break;
+	case 2:
+		inputPoint(pointRecord, filledPoints);
+		point3 = pointRecord[2];
+		break;
+	default:
+		point2 = choosePoint(pointRecord, filledPoints);
+		point3 = choosePoint(pointRecord, filledPoints);
+		break;
+	}
+}
+
 Line chooseLine(Line* lineRecord, int& filledLines)
 {
 	Line line_temp{};
@@ -195,7 +274,7 @@ void equationOfParallelLine(Point pointRecord[], Line lineRecord[], int& filledP
 	Point point = choosePoint(pointRecord, filledPoints);
 	Line line = chooseLine(lineRecord, filledLines);
 
-	int k_new = point.y - line.m * point.x;
+	double k_new = point.y - line.m * point.x; //find out the new k from the x and y from the given point
 	if (k_new == line.k)
 	{
 		std::cout << "The point lies on the line already. The requested parallel line is the same as the given.";
@@ -204,8 +283,134 @@ void equationOfParallelLine(Point pointRecord[], Line lineRecord[], int& filledP
 
 	std::cout << "The equation of a line that is parallel to the given line and passes through the given point is: ";
 	std::cout << -line.m << "x + y + " << -k_new << " = 0" << std::endl;
+	//the parralel line has the same slope and the new k
 }
 
+void equationOfPerpendicularLine(Point pointRecord[], Line lineRecord[], int& filledPoints, int& filledLines)
+{
+	Point point = choosePoint(pointRecord, filledPoints);
+	Line line = chooseLine(lineRecord, filledLines);
+
+	double m_new = -1.0 / line.m;
+	double k_new = point.y - m_new * point.x; //find out the new k from the x and y from the given point
+
+	std::cout << "The equation of a line that is perpendicular to the given line and passes through the given point is: ";
+	std::cout << -m_new << "x + y + " << -k_new << " = 0" << std::endl;
+}
+
+void intersectionOfTwoLines(Line lineRecord[], int& filledLines)
+{
+	Line line1 = chooseLine(lineRecord, filledLines);
+	Line line2{};
+	if (filledLines == 1)
+	{
+		inputLine(lineRecord, filledLines);
+		line2 = lineRecord[1]; //the element on first index is exactly the one we entered if the lines are not enough
+	}
+	else
+	{
+		line2 = chooseLine(lineRecord, filledLines);
+	}
+	Point point{};
+
+	if (line1.m == line2.m)
+	{
+		std::cout << "The lines are parallel and do not intersect." << std::endl;
+		return;
+	}
+
+	point.x = (line2.k - line1.k) / (line1.m - line2.m);
+	point.y = line1.m * point.x + line1.k;
+
+	std::cout << "The intersection point of the lines is (" << point.x << ", " << point.y << ")";
+}
+
+void equationsOfAltitudes(Point pointRecord[], Line lineRecord[], int& filledPoints, int& filledLines)
+{
+	Point point1{};
+	Point point2{};
+	Point point3{};
+	chooseThreeValidPoints(pointRecord, filledPoints, point1, point2, point3);
+	if (!isTriangleExist(point1, point2, point3))
+	{
+		std::cout << "Тhese three points do not form a triangle.";
+		return;
+	}
+	std::cout << std::endl;
+
+	Line altitudeFrom1 = altitude(point2, point3, point1); //altitude that goes from point1 to line through point2 and point3
+	std::cout << "The equation of the altitude that goes from " << point1.pointName << " in the triangle is: ";
+	std::cout << altitudeFrom1.a << "x - " << altitudeFrom1.b << "y + " << altitudeFrom1.c << " = 0" << std::endl;
+	std::cout << std::endl;
+
+	Line altitudeFrom2 = altitude(point1, point3, point2); //altitude that goes from point2 to line through point1 and point3
+	std::cout << "The equation of the altitude that goes from " << point2.pointName << " in the triangle is: ";
+	std::cout << altitudeFrom2.a << "x - " << altitudeFrom2.b << "y + " << altitudeFrom2.c << " = 0" << std::endl;
+	std::cout << std::endl;
+
+	Line altitudeFrom3 = altitude(point1, point2, point3); //altitude that goes from point3 to line through point1 and point2
+	std::cout << "The equation of the altitude that goes from " << point3.pointName << " in the triangle is: ";
+	std::cout << altitudeFrom3.a << "x - " << altitudeFrom3.b << "y + " << altitudeFrom3.c << " = 0" << std::endl;
+	std::cout << std::endl;
+}
+
+void equationsOfMedians(Point pointRecord[], Line lineRecord[], int& filledPoints, int& filledLines)
+{
+	Point point1{};
+	Point point2{};
+	Point point3{};
+	chooseThreeValidPoints(pointRecord, filledPoints, point1, point2, point3);
+	if (!isTriangleExist(point1, point2, point3)) 
+	{
+		std::cout << "Тhese three points do not form a triangle.";
+		return;
+	}
+	std::cout << std::endl;
+
+	Line medianFrom1 = median(point2, point3, point1); //the median that is from point1 to the line through point2 and point3
+	std::cout << "The equation of the median that goes from " << point1.pointName << " in the triangle is: ";
+	std::cout << medianFrom1.a << "x + " << medianFrom1.b << "y + " << medianFrom1.c << " = 0" << std::endl;
+	std::cout << std::endl;
+
+	Line medianFrom2 = median(point1, point3, point2); //the median that is from point2 to the line through point1 and point3
+	std::cout << "The equation of the median that goes from " << point2.pointName << " in the triangle is: ";
+	std::cout << medianFrom2.a << "x + " << medianFrom2.b << "y + " << medianFrom2.c << " = 0" << std::endl;
+	std::cout << std::endl;
+
+	Line medianFrom3 = median(point1, point2, point3); //the median that is from point3 to the line through point1 and point2
+	std::cout << "The equation of the median that goes from " << point3.pointName << " in the triangle is: ";
+	std::cout << medianFrom3.a << "x + " << medianFrom3.b << "y + " << medianFrom3.c << " = 0" << std::endl;
+	std::cout << std::endl;
+}
+
+void equationsOfperpendicularBisectors(Point pointRecord[], Line lineRecord[], int& filledPoints, int& filledLines)
+{
+	Point point1{};
+	Point point2{};
+	Point point3{};
+	chooseThreeValidPoints(pointRecord, filledPoints, point1, point2, point3);
+	if (!isTriangleExist(point1, point2, point3))
+	{
+		std::cout << "Тhese three points do not form a triangle.";
+		return;
+	}
+	std::cout << std::endl;
+
+	Line perpBisectorThrough12 = perpBisector(point1, point2); //the perpendicular bisector that passes the line through point1 and point2
+	std::cout << "The equation of the perpendicular bisector that passes the line through " << point1.pointName << " and "<<point2.pointName <<" in the triangle is: ";
+	std::cout << perpBisectorThrough12.a << "x + " << perpBisectorThrough12.b << "y + " << perpBisectorThrough12.c << " = 0" << std::endl;
+	std::cout << std::endl;
+
+	Line perpBisectorThrough23 = perpBisector(point2, point3); //the perpendicular bisector that passes the line through point2 and point3
+	std::cout << "The equation of the perpendicular bisector that passes the line through " << point2.pointName << " and " << point3.pointName << " in the triangle is: ";
+	std::cout << perpBisectorThrough23.a << "x + " << perpBisectorThrough23.b << "y + " << perpBisectorThrough23.c << " = 0" << std::endl;
+	std::cout << std::endl;
+
+	Line perpBisectorThrough31 = perpBisector(point3, point1); //the perpendicular bisector that passes the line through point3 and point1
+	std::cout << "The equation of the perpendicular bisector that passes the line through " << point3.pointName << " and " << point1.pointName << " in the triangle is: ";
+	std::cout << perpBisectorThrough31.a << "x + " << perpBisectorThrough31.b << "y + " << perpBisectorThrough31.c << " = 0" << std::endl;
+	std::cout << std::endl;
+}
 int main()
 {
 	Point pointRecord[SIZE];
@@ -253,14 +458,24 @@ int main()
 			std::cout << std::endl;
 			break;
 		case 5:
+			equationOfPerpendicularLine(pointRecord, lineRecord, filledPoints, filledLines);
+			std::cout << std::endl;
 			break;
 		case 6:
+			intersectionOfTwoLines(lineRecord, filledLines);
+			std::cout << std::endl;
 			break;
 		case 7:
+			equationsOfAltitudes(pointRecord, lineRecord, filledPoints, filledLines);
+			std::cout << std::endl;
 			break;
 		case 8:
+			equationsOfMedians(pointRecord, lineRecord, filledPoints, filledLines);
+			std::cout << std::endl;
 			break;
 		case 9:
+			equationsOfperpendicularBisectors(pointRecord, lineRecord, filledPoints, filledLines);
+			std::cout << std::endl;
 			break;
 		case 10:
 			break;
@@ -280,7 +495,5 @@ int main()
 
 	} while (choice != 13);
 
-	return 0;
-}
 	return 0;
 }
